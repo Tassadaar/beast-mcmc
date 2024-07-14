f2c5560e2d99225fd05a4c575dee169df5bbd70b
Jason Shao <Jason.Shao@dal.ca>
2024-07-14 19:55:59 -0300

    refactoring: long method BeastDialog() in BeastDialog.java

--- src/dr/app/beast/BeastDialog.java
+++ src/dr/app/beast/BeastDialog.java
@@ -70,16 +70,20 @@ public class BeastDialog {
 
     public BeastDialog(final JFrame frame, final String titleString, final Icon icon) {
         this.frame = frame;
+        this.optionPanel = new OptionsPanel(12, 12);
 
-        optionPanel = new OptionsPanel(12, 12);
+        initializeMainPanel(icon, titleString);
+        initializeInputFileSection();
+        initializeCheckpointingSection();
+        initializeAdditionalOptions();
+        initializeBeagleOptions();
+    }
 
-        //this.frame = frame;
-
+    private void initializeMainPanel(final Icon icon, final String titleString) {
         JPanel panel = new JPanel(new BorderLayout());
         panel.setOpaque(false);
 
         final OptionsPanel optionPanel3 = new OptionsPanel(0, 3);
-
         final JLabel titleIcon = new JLabel();
         titleIcon.setIcon(icon);
 
@@ -89,140 +93,54 @@ public class BeastDialog {
         titleText.addHyperlinkListener(new SimpleLinkListener());
         optionPanel3.addComponent(titleText);
 
-//        final JButton aboutButton = new JButton("About BEAST...");
-//        //aboutButton.setAction();
-//        optionPanel3.addComponent(aboutButton);
-
         optionPanel.addComponents(titleIcon, optionPanel3);
+    }
 
+    private void initializeInputFileSection() {
         final JButton inputFileButton = new JButton("Choose File...");
         final JTextField inputFileNameText = new JTextField("not selected", 16);
 
         inputFileButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-                FileDialog dialog = new FileDialog(frame,
-                        "Select target file...",
-                        FileDialog.LOAD);
-
+                FileDialog dialog = new FileDialog(frame, "Select target file...", FileDialog.LOAD);
                 dialog.setVisible(true);
                 if (dialog.getFile() == null) {
-                    // the dialog was cancelled...
                     return;
                 }
-
                 inputFile = new File(dialog.getDirectory(), dialog.getFile());
                 inputFileNameText.setText(inputFile.getName());
-
             }
         });
+
         inputFileNameText.setEditable(false);
+        setupInputFileDragAndDrop(inputFileNameText);
 
         JPanel panel1 = new JPanel(new BorderLayout(0, 0));
         panel1.add(inputFileNameText, BorderLayout.CENTER);
         panel1.add(inputFileButton, BorderLayout.EAST);
-        inputFileNameText.setToolTipText(
-                "<html>Drag a BEAST XML file here or use the button to<br>" +
-                        "select one from a file dialog box.</html>");
-        inputFileButton.setToolTipText(
-                "<html>Drag a BEAST XML file here or use the button to<br>" +
-                        "select one from a file dialog box.</html>");
+        inputFileNameText.setToolTipText("<html>Drag a BEAST XML file here or use the button to<br>select one from a file dialog box.</html>");
+        inputFileButton.setToolTipText("<html>Drag a BEAST XML file here or use the button to<br>select one from a file dialog box.</html>");
         optionPanel.addComponentWithLabel("BEAST XML File: ", panel1);
+    }
 
+    private void setupInputFileDragAndDrop(final JTextField inputFileNameText) {
         Color focusColor = UIManager.getColor("Focus.color");
         Border focusBorder = BorderFactory.createMatteBorder(2, 2, 2, 2, focusColor);
         new FileDrop(null, inputFileNameText, focusBorder, new FileDrop.Listener() {
             public void filesDropped(java.io.File[] files) {
                 inputFile = files[0];
                 inputFileNameText.setText(inputFile.getName());
-            }   // end filesDropped
-        }); // end FileDrop.Listener
+            }
+        });
+    }
 
-        overwriteCheckBox.setToolTipText(
-                "<html>Specify whether BEAST will overwrite existing log files<br>" +
-                      "with the same name.</html>");
-        optionPanel.addComponent(overwriteCheckBox);
-
-        optionPanel.addSeparator();
-
+    private void initializeCheckpointingSection() {
         final JButton chkptButton = new JButton("Custom settings");
-        chkptButton.setToolTipText("<html>By default, a checkpoint file will be written according to the<br>" +
-                "specifications in your XML file. No previous checkpointed file<br>" +
-                "will be loaded and no custom file name can be provided.</html>");
+        chkptButton.setToolTipText("<html>By default, a checkpoint file will be written according to the<br>specifications in your XML file. No previous checkpointed file<br>will be loaded and no custom file name can be provided.</html>");
 
         chkptButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                JDialog dialog = new JDialog(frame, "Checkpointing settings");
-                dialog.setLocationRelativeTo(chkptButton);
-                dialog.setModal(true);
-                dialog.setAlwaysOnTop(true);
-
-                OptionsPanel chkptPanel = new OptionsPanel(0, 3);
-                //chkptPanel.setLayout(new BorderLayout());
-                chkptPanel.setOpaque(false);
-                JCheckBox overruleXML = new JCheckBox();
-                overruleXML.setToolTipText("This will ignore the checkpointing settings in your XML file.");
-                overruleXML.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent actionEvent) {
-                        if (overruleXML.isSelected()) {
-                            System.setProperty("checkpointOverrule", Boolean.TRUE.toString());
-                        } else {
-                            System.setProperty("checkpointOverrule", Boolean.FALSE.toString());
-                        }
-                    }
-                });
-                chkptPanel.addComponentWithLabel("Overrule XML checkpointing settings", overruleXML);
-
-                chkptPanel.addSeparator();
-
-                JTextField checkpointInput = new JTextField(15);
-                checkpointInput.addKeyListener(new java.awt.event.KeyListener() {
-                    public void keyTyped(KeyEvent e) {
-                    }
-
-                    public void keyPressed(KeyEvent e) {
-                    }
-
-                    public void keyReleased(KeyEvent e) {
-                        System.setProperty(BeastCheckpointer.LOAD_STATE_FILE, checkpointInput.getText());
-                    }
-                });
-
-                JTextField checkpointOutput = new JTextField(15);
-                checkpointOutput.addKeyListener(new java.awt.event.KeyListener() {
-                    public void keyTyped(KeyEvent e) {
-                    }
-
-                    public void keyPressed(KeyEvent e) {
-                    }
-
-                    public void keyReleased(KeyEvent e) {
-                        System.setProperty(BeastCheckpointer.SAVE_STATE_FILE, checkpointOutput.getText());
-                    }
-                });
-
-                WholeNumberField checkpointEvery = new WholeNumberField(1, Integer.MAX_VALUE);
-                checkpointEvery.addKeyListener(new java.awt.event.KeyListener() {
-                    public void keyTyped(KeyEvent e) {
-                    }
-
-                    public void keyPressed(KeyEvent e) {
-                    }
-
-                    public void keyReleased(KeyEvent e) {
-                        System.setProperty(BeastCheckpointer.SAVE_STATE_EVERY, checkpointEvery.getValue() + "");
-                    }
-                });
-                checkpointEvery.setValue(1000000);
-                checkpointEvery.setColumns(10);
-
-                chkptPanel.addComponentWithLabel("Load previous checkpoint file: ", checkpointInput);
-                chkptPanel.addComponentWithLabel("Save new checkpoint file (will overwrite): ", checkpointOutput);
-                chkptPanel.addComponentWithLabel("Save checkpoint every: ", checkpointEvery);
-
-                dialog.add(chkptPanel);
-                dialog.pack();
-                dialog.setVisible(true);
+                showCheckpointSettingsDialog(chkptButton);
             }
         });
 
@@ -231,31 +149,91 @@ public class BeastDialog {
         optionPanel.addComponentWithLabel("Checkpointing:", chkptPanel);
 
         optionPanel.addSeparator();
+    }
 
+    private void showCheckpointSettingsDialog(final JButton chkptButton) {
+        JDialog dialog = new JDialog(frame, "Checkpointing settings");
+        dialog.setLocationRelativeTo(chkptButton);
+        dialog.setModal(true);
+        dialog.setAlwaysOnTop(true);
+
+        OptionsPanel chkptPanel = new OptionsPanel(0, 3);
+        chkptPanel.setOpaque(false);
+        JCheckBox overruleXML = new JCheckBox();
+        overruleXML.setToolTipText("This will ignore the checkpointing settings in your XML file.");
+        overruleXML.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.setProperty("checkpointOverrule", Boolean.toString(overruleXML.isSelected()));
+            }
+        });
+        chkptPanel.addComponentWithLabel("Overrule XML checkpointing settings", overruleXML);
+
+        chkptPanel.addSeparator();
+
+        JTextField checkpointInput = createCheckpointTextField(BeastCheckpointer.LOAD_STATE_FILE);
+        JTextField checkpointOutput = createCheckpointTextField(BeastCheckpointer.SAVE_STATE_FILE);
+        WholeNumberField checkpointEvery = createCheckpointEveryField();
+
+        chkptPanel.addComponentWithLabel("Load previous checkpoint file: ", checkpointInput);
+        chkptPanel.addComponentWithLabel("Save new checkpoint file (will overwrite): ", checkpointOutput);
+        chkptPanel.addComponentWithLabel("Save checkpoint every: ", checkpointEvery);
+
+        dialog.add(chkptPanel);
+        dialog.pack();
+        dialog.setVisible(true);
+    }
+
+    private JTextField createCheckpointTextField(final String property) {
+        JTextField checkpointTextField = new JTextField(15);
+        checkpointTextField.addKeyListener(new java.awt.event.KeyListener() {
+            public void keyTyped(KeyEvent e) {
+            }
+
+            public void keyPressed(KeyEvent e) {
+            }
+
+            public void keyReleased(KeyEvent e) {
+                System.setProperty(property, checkpointTextField.getText());
+            }
+        });
+        return checkpointTextField;
+    }
+
+    private WholeNumberField createCheckpointEveryField() {
+        WholeNumberField checkpointEvery = new WholeNumberField(1, Integer.MAX_VALUE);
+        checkpointEvery.addKeyListener(new java.awt.event.KeyListener() {
+            public void keyTyped(KeyEvent e) {
+            }
+
+            public void keyPressed(KeyEvent e) {
+            }
+
+            public void keyReleased(KeyEvent e) {
+                System.setProperty(BeastCheckpointer.SAVE_STATE_EVERY, checkpointEvery.getValue() + "");
+            }
+        });
+        checkpointEvery.setValue(1000000);
+        checkpointEvery.setColumns(10);
+        return checkpointEvery;
+    }
+
+    private void initializeAdditionalOptions() {
         seedText.setColumns(12);
-        seedText.setToolTipText(
-                "<html>Specify a particular random number seed to replicate<br>" +
-                "precisely the sequence of steps in the MCMC chain. By<br>" +
-                "default this uses system information to provide a new<br>" +
-                        "seed each run.</html>");
-
+        seedText.setToolTipText("<html>Specify a particular random number seed to replicate<br>precisely the sequence of steps in the MCMC chain. By<br>default this uses system information to provide a new<br>seed each run.</html>");
         optionPanel.addComponentWithLabel("Random number seed: ", seedText);
 
-        threadsCombo.setToolTipText(
-                "<html>Specify how large a thread pool to use.<br>" +
-                      "In most circumstances this should be set to 'automatic'<br>" +
-                      "but in some circumstances it may be desirable to restict<br>" +
-                      "the number of cores being used. 0 will turn off threading</html>");
-
+        threadsCombo.setToolTipText("<html>Specify how large a thread pool to use.<br>In most circumstances this should be set to 'automatic'<br>but in some circumstances it may be desirable to restict<br>the number of cores being used. 0 will turn off threading</html>");
         optionPanel.addComponentWithLabel("Thread pool size: ", threadsCombo);
 
         optionPanel.addSeparator();
 
         optionPanel.addSpanningComponent(beagleCheckBox);
         beagleCheckBox.setSelected(true);
+    }
 
+    private void initializeBeagleOptions() {
         final OptionsPanel optionPanel1 = new OptionsPanel(0, 6);
-//        optionPanel1.setBorder(BorderFactory.createEmptyBorder());
         optionPanel1.setBorder(new TitledBorder(""));
 
         OptionsPanel optionPanel2 = new OptionsPanel(0, 3);
@@ -270,6 +248,19 @@ public class BeastDialog {
 
         optionPanel1.addComponent(optionPanel2);
 
+        final JEditorPane beagleInfo = createBeagleInfoPane();
+        optionPanel1.addComponent(beagleInfo);
+        optionPanel1.setBorder(BorderFactory.createEmptyBorder());
+        optionPanel.addSpanningComponent(optionPanel1);
+
+        setupBeagleCheckBoxListeners(beagleInfo, label1, label2, label3);
+
+        beagleCheckBox.setSelected(true);
+        beagleCheckBox.setEnabled(false);
+        beagleResourceCombo.setSelectedItem("CPU");
+    }
+
+    private JEditorPane createBeagleInfoPane() {
         final JEditorPane beagleInfo = new JEditorPane("text/html",
                 "<html><div style=\"font-family:'helvetica neue light',helvetica,sans-serif;font-size:12;\"><p>BEAGLE is a high-performance phylogenetic library that can make use of<br>" +
                         "additional computational resources such as graphics boards. It must be<br>" +
@@ -278,28 +269,24 @@ public class BeastDialog {
         beagleInfo.setOpaque(false);
         beagleInfo.setEditable(false);
         beagleInfo.addHyperlinkListener(new SimpleLinkListener());
-        optionPanel1.addComponent(beagleInfo);
-        optionPanel1.setBorder(BorderFactory.createEmptyBorder());
-        optionPanel.addSpanningComponent(optionPanel1);
+        return beagleInfo;
+    }
 
-        beagleInfoCheckBox.setEnabled(false);
+    private void setupBeagleCheckBoxListeners(final JEditorPane beagleInfo, final JLabel label1, final JLabel label2, final JLabel label3) {
         beagleCheckBox.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
-                beagleInfo.setEnabled(beagleCheckBox.isSelected());
-                beagleInfoCheckBox.setEnabled(beagleCheckBox.isSelected());
-                label1.setEnabled(beagleCheckBox.isSelected());
-                beagleResourceCombo.setEnabled(beagleCheckBox.isSelected());
-                beagleSSECheckBox.setEnabled(beagleCheckBox.isSelected());
-                label2.setEnabled(beagleCheckBox.isSelected());
-                beaglePrecisionCombo.setEnabled(beagleCheckBox.isSelected());
-                label3.setEnabled(beagleCheckBox.isSelected());
-                beagleScalingCombo.setEnabled(beagleCheckBox.isSelected());
+                boolean selected = beagleCheckBox.isSelected();
+                beagleInfo.setEnabled(selected);
+                beagleInfoCheckBox.setEnabled(selected);
+                label1.setEnabled(selected);
+                beagleResourceCombo.setEnabled(selected);
+                beagleSSECheckBox.setEnabled(selected);
+                label2.setEnabled(selected);
+                beaglePrecisionCombo.setEnabled(selected);
+                label3.setEnabled(selected);
+                beagleScalingCombo.setEnabled(selected);
             }
         });
-
-        beagleCheckBox.setSelected(true);
-        beagleCheckBox.setEnabled(false);
-        beagleResourceCombo.setSelectedItem("CPU");
     }
 
     public boolean showDialog(String title) {
